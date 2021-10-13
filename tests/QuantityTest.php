2d1adaead2add5ef9ecb6bcf105e389b0c54e5d5
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2021-10-13 22:30:33 +0200

    Add more tests for add and subtract

--- tests/QuantityTest.php
+++ tests/QuantityTest.php
@@ -27,4 +27,51 @@ class QuantityTest extends TestCase
         yield 'decimal float' => ['.99'];
         yield 'exponent' => ['-1.3e3'];
     }
+
+    public function testQuantityInvalidNumericAmount(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Quantity::fromString('hello');
+    }
+
+    /**
+     * @dataProvider addProvider
+     */
+    public function testAdd(string $base, string $addend, string $result): void
+    {
+        $base = Quantity::fromString($base);
+        $sum = $base->add(Quantity::fromString($addend));
+
+        self::assertSame($result, $sum->amount());
+        self::assertNotSame($base, $sum);
+    }
+
+    /**
+     * @return iterable<array<string>>
+     */
+    public function addProvider(): iterable
+    {
+        yield 'integers' => ['0', '1', '1'];
+    }
+
+    /**
+     * @dataProvider subtractProvider
+     */
+    public function testSubtract(string $base, string $subtrahend, string $result): void
+    {
+        $base = Quantity::fromString($base);
+        $difference = $base->subtract(Quantity::fromString($subtrahend));
+
+        self::assertSame($result, $difference->amount());
+        self::assertNotSame($base, $difference);
+    }
+
+    /**
+     * @return iterable<array<string>>
+     */
+    public function subtractProvider(): iterable
+    {
+        yield 'integers' => ['5', '4', '1'];
+    }
 }
